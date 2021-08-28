16e64bd567b43b78b1c0b0ba4826c7ca5a70f248
Jonatandlr <a01735993@itesm.mx>
2021-08-27 19:01:14 -0500

    holi

--- assignments/02Ordena/src/exercise.py
+++ assignments/02Ordena/src/exercise.py
@@ -4,6 +4,28 @@ def main():
     y = int(input("Ingresa el segundo número: "))
     z = int(input("Ingresa el tercer número: "))
     
+    if x>y:
+        if y>z:
+            print (x)
+            print(y)
+            print (z)
+        elif x>z:
+            print(x)
+            print(z)
+            print(y)
+    elif x>z:
+        print(y)
+        print(x)
+        print(z)
+    elif z>y:
+        print(z)
+        print(y)
+        print(x)
+    else:
+        print(y)
+        print(z)
+        print(x)
+        
 
 
 if __name__=='__main__':
