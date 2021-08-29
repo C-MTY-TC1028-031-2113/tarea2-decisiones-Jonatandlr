2bdb65d07ff8403d10f296e21911373c44dc02b0
Jonatandlr <a01735993@itesm.mx>
2021-08-29 14:13:07 -0500

    Terminado

--- assignments/04Cuadrante/src/exercise.py
+++ assignments/04Cuadrante/src/exercise.py
@@ -1,6 +1,20 @@
 def main():
     # Escribe tu código abajo de esta línea
-    pass
+    grados= int(input("Introduce los grados: "))
+
+    if 0<=grados<=360:
+        if 0<grados<90:
+            print("cuadrante 1")
+        elif 90<grados<180:
+            print("cuadrante 2")
+        elif 180<grados<270:
+            print("cuadrante 3")
+        elif 270<grados<360:
+            print("cuadrante 4")
+        else:
+            print("eje")
+    else: 
+        print("exede")
 
 if __name__ == '__main__':
     main()
--- assignments/05Cuadratica/src/exercise.py
+++ assignments/05Cuadratica/src/exercise.py
@@ -5,7 +5,25 @@ def main():
     a=int(input("Da el valor de a: "))
     b=int(input("Da el valor de b: "))
     c=int(input("Da el valor de c: "))
-    pass
+    
+    if a==0 and b==0:
+        print("No tiene solucion")
+    elif a==0 and b!=0:
+        x=float(-c/b)
+        print (x)
+    elif a!=0 and b!=0:
+        discriminante= b**2-4*a*c
+        if discriminante<0:
+            print("Raices complejas")
+        elif discriminante>0:
+            x1=(-b+discriminante)/(2**a)
+            x2=(-b-discriminante)/(2**a)
+            print(x1)
+            print(x2)
+        else: 
+            print(-b/2*a)
+        
+        
 
 if __name__ == '__main__':
     main()
